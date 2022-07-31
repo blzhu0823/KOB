47bacc7bc405b0b89ddeec045a048b1d43feb8f8
Bolin Zhu <blzhu0823@gmail.com>
2022-07-31 11:19:05 +0800

    基础前后端分离demo

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,26 +4,19 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo() {
-        List<Map<String, String>> botInfo = new LinkedList<>();
-        botInfo.add(new HashMap<String, String>() {{
-            put("name", "bot1");
-            put("ip", "localhost");
-            put("port", "8080");
-        }});
-        botInfo.add(new HashMap<String, String>() {{
-            put("name", "bot2");
-            put("ip", "localhost");
-            put("port", "8081");
-        }});
+    public Map<String, String> getBotInfo() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "haha");
+        map.put("age", "18");
 
-        return botInfo;
+        return map;
     }
 }
