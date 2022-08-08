2798d169df1058c5c6f1f602740f9758716873aa
Bolin Zhu <blzhu0823@gmail.com>
2022-08-09 02:48:35 +0800

    spring连接mysql，实现登陆安全模块

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,17 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PasswordEncoder pe = new BCryptPasswordEncoder();
+		System.out.println(pe.encode("woaini123"));
+		System.out.println(pe.matches("woaini123", "$2a$10$AMnfvLWsuCP5uG0h4DoTeeGtQ5L6Rm.96kuD3lLNr/pJxnSpfYXN."));
 	}
 
 }
